4247552775e256acfe33e9e9888a643cbff1d514
Igor432 <igomar0602@gmail.com>
2022-07-21 18:48:30 +0300

    final

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -9,36 +9,32 @@ const countryInfo = document.querySelector('.country-info');
 
 
 
-
-
 function fetchCountries(name) {
 
     return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages,currencies`)
         .then(response => {
             if (!response.ok) {
-                throw new Error(response.status)
+
+                Notiflix.Notify.failure("Oops, there is no country with that name")
+
             }
             return response.json()
         }
         )
-
 }
-
 
 function makeList(countries) {
 
-
     console.log(countries)
-
 
     if (countries.length > 10) {
         Notiflix.Notify.info('"Too many matches found. Please enter a more specific name."');
-    } else if (countries.length > 2 && countries.length < 10) {
+    } else if (countries.length >= 2 && countries.length < 10) {
         const list = countries
             .map(({ name, flags }) => {
                 return `
             <li> <img src=${flags.svg} height='20px' width='30px'> ${name} </li>`
-            })
+            }).join("")
         countryInfo.innerHTML = list;
     } else {
         const list = countries
@@ -50,13 +46,10 @@ function makeList(countries) {
             <li>Languages: ${languages[0].name}</li>
             <li>Currency: ${currencies[0].name}</li>
             <li> <img src=${flags.svg} height='60px' width='90px'></li>`
-            })
+            }).join("");
         countryInfo.innerHTML = list;
     }
 }
-
-
-
 
 
 countryInput.addEventListener('input', debounce(() => {
@@ -69,8 +62,6 @@ countryInput.addEventListener('input', debounce(() => {
             .then((countries) => makeList(countries))
 
     }
-
-
 }, 300))
 
 
