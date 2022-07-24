a70f76896dc097c579f7a6ee775df6f068ad5f29
Igor432 <igomar0602@gmail.com>
2022-07-24 17:15:54 +0300

    fixed

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -8,8 +8,8 @@ function fetchCountries(name) {
     return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages,currencies`)
         .then(response => {
             if (!response.ok) {
+                Notiflix.Notify.failure('Oops, there is no country with that name')
 
-                Notiflix.Notify.failure('Oops, there is no country with that name');
             }
             return response.json()
         }
--- src/js/main.js
+++ src/js/main.js
@@ -9,37 +9,43 @@ const countryInfo = document.querySelector('.country-info');
 
 
 function makeList(countries) {
+
     let allLang = [];
+    let list = '';
     console.log(countries)
 
     if (countries.length > 10) {
         Notiflix.Notify.info('"Too many matches found. Please enter a more specific name."');
     } else if (countries.length >= 2 && countries.length <= 10) {
-        const list = countries
+        list = countries
             .map(({ name, flags }) => {
+
                 return `
             <li> <img src=${flags.svg} height='20px' width='30px'> ${name} </li>`
             }).join("")
         countryInfo.innerHTML = list;
 
-    } else {
+    } else if (countries.length === 1) {
+        list = countries
+            .map((country) => {
 
+                for (let lang of country.languages) {
+                    const langs = Object.values(lang)
+                    allLang.push(langs[3])
 
-        const list = countries.map(({ name, capital, population, flags, languages, currencies }) => {
-            for (let lang of languages) {
-                allLang.push(lang.name)
-            }
+                }
 
-            return `<li class='list_item'> <img src=${flags.svg} height='40px' width='60px'><span class='list_title name'> ${name}</span></li>
-            <li class='list_item'><span class='list_title'>Capital:</span> ${capital}</li>
-            <li class='list_item'><span class='list_title'>Population:</span> ${population} </li>
+                return `<li class='list_item'> <img src=${country.flags.svg} height='40px' width='60px'><span class='list_title name'> ${country.name}</span></li>
+            <li class='list_item'><span class='list_title'>Capital:</span> ${country.capital}</li>
+            <li class='list_item'><span class='list_title'>Population:</span> ${country.population} </li>
             
             <li class='list_item'><span class='list_title'>Languages:</span> ${allLang.join(', ')}</li >
-            <li class='list_item'><span class='list_title'>Currency:</span> ${currencies[0].name}</li>
+            <li class='list_item'><span class='list_title'>Currency:</span> ${country.currencies[0].name}</li>
             `
-        }).join("");
-        countryInfo.innerHTML = list;
+            }).join("");
+
     }
+    countryInfo.innerHTML = list;
 }
 
 
@@ -50,7 +56,8 @@ countryInput.addEventListener('input', debounce(() => {
         countryInfo.innerHTML = '';
     } else {
         fetchCountries(inputText)
-            .then((countries) => makeList(countries))
-            .catch(error => console.log(error));
+            .then(countries => makeList(countries))
+
+            ;
     }
 }, 300))
